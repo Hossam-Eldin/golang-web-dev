a177b914e4afef34014748394d44c81d7765fa20
Hossam-Elden <hosam.elden@hotmail.com>
2017-12-16 17:04:25 +0200

    300 fam

---
+++ 16 passing_data/05 form_file/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/",file)
+	http.ListenAndServe(":8080", nil)
+}
+
+func file(w  http.ResponseWriter, req *http.Request){
+var s string
+	if req.Method == http.MethodPost{
+		//open the file
+		f, h, err :=req.FormFile("q")
+		if err !=nil{
+			http.Error(w,err.Error(),http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		// for information
+
+		fmt.Println("\nfile:", f, "\nheader:", h, "\nerr", err)
+
+		//read the file
+		bs, err := ioutil.ReadAll(f)
+		if err !=nil{
+			http.Error(w,err.Error(),http.StatusInternalServerError)
+			return
+		}
+		s = string(bs)
+
+		//store in server
+		dst, err := os.Create(filepath.Join("./user/", h.Filename))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer dst.Close()
+		_, err = dst.Write(bs)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tpl.ExecuteTemplate(w, "index.html", s)
+}
---
+++ 17 redirect/01 303_SeeOther/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"html/template"
+	"log"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", one)
+	http.HandleFunc("/two", tow)
+	http.HandleFunc("/there", there)
+	http.ListenAndServe(":8080", nil)
+}
+
+func one(w http.ResponseWriter, req *http.Request) {
+	fmt.Print("you request method at one:", req.Method, "\n\n")
+}
+
+func tow(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Your request method at bar:", req.Method)
+	// process form submission here
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
+
+func there(w http.ResponseWriter, req *http.Request) {
+	fmt.Print("you request methods at there", req.Method, "\n\n")
+	err := tpl.ExecuteTemplate(w, "index.html", req.Method)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
