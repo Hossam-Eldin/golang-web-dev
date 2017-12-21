8775f459088db538d78fedbd1f11fd17bfddc663
Hossam-Elden <hosam.elden@hotmail.com>
2017-12-21 17:59:15 +0200

    mine blog starting

---
+++ 020 mine_blog/02 cookies/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"github.com/satori/go.uuid"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	c :=getCookie(w,req)
+	tpl.ExecuteTemplate(w, "index.html", c.Value)
+}
+
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie  {
+	c, err := req.Cookie("session")
+
+	if err != nil {
+		sId := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sId.String(),
+		}
+		http.SetCookie(w,c)
+	}
+
+	return c
+}
---
+++ 020 mine_blog/03 store_value/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"github.com/satori/go.uuid"
+	"strings"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	c := getCookie(w, req)
+	c = appenVlue(w, c)
+	xs := strings.Split(c.Value, "|")
+	tpl.ExecuteTemplate(w, "index.html", xs)
+}
+
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("session")
+
+	if err != nil {
+		sId := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sId.String(),
+		}
+		http.SetCookie(w, c)
+	}
+
+	return c
+}
+
+func appenVlue(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
+	// values
+	p1 := "disneyland.jpg"
+	p2 := "atbeach.jpg"
+	p3 := "hollywood.jpg"
+
+	// append
+	s := c.Value
+	if !strings.Contains(s, p1) {
+		s += "|" + p1
+	}
+	if !strings.Contains(s, p2) {
+		s += "|" + p2
+	}
+	if !strings.Contains(s, p3) {
+		s += "|" + p3
+	}
+	c.Value = s
+	http.SetCookie(w, c)
+	return c
+
+}
