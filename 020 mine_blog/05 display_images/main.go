df9e39391b3db99216eece93ae590e83b0d3583d
Hossam-Elden <hosam.elden@hotmail.com>
2017-12-23 13:50:08 +0200

    web tools

---
+++ 020 mine_blog/05 display_images/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"github.com/satori/go.uuid"
+	"strings"
+	"log"
+	"crypto/sha1"
+	"io"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	c := getCookie(w, req)
+
+	if req.Method == http.MethodPost{
+		mf , fh ,err:=req.FormFile("nf")
+		if err !=nil{
+			log.Fatalln(err)
+		}
+		defer mf.Close()
+		// create sha for file name
+		ext := strings.Split(fh.Filename, ".")[1]
+		h := sha1.New()
+		io.Copy(h, mf)
+		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+
+		// create new file
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		path := filepath.Join(wd, "public", "pics", fname)
+		nf, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer nf.Close()
+		// copy
+		mf.Seek(0, 0)
+		io.Copy(nf, mf)
+		// add filename to this user's cookie
+		c = appenVlue(w,c,fname)
+	}
+
+
+	xs := strings.Split(c.Value, "|")
+	tpl.ExecuteTemplate(w, "index.html", xs[1:])
+}
+
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("session")
+
+	if err != nil {
+		sId := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sId.String(),
+		}
+		http.SetCookie(w, c)
+	}
+
+	return c
+}
+
+func appenVlue(w http.ResponseWriter, c *http.Cookie ,fname string) *http.Cookie {
+
+	// append
+	s := c.Value
+	if !strings.Contains(s, fname) {
+		s += "|" + fname
+	}
+	c.Value = s
+	http.SetCookie(w, c)
+	return c
+
+}
